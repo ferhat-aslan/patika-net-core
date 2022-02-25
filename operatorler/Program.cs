b5d2fcd937c8c812c58fe22b2025d787f6c3d107
ferhat-aslan <aslanferhat16@gmail.com>
2022-02-25 20:39:32 +0300

    dsfd

--- operatorler/Program.cs
+++ operatorler/Program.cs
@@ -1,22 +1,43 @@
-﻿int time = DateTime.Now.Hour;
-
-            if (time >= 6 && time <= 11)
+﻿int ay = DateTime.Now.Month;
+            switch (ay)
             {
-                Console.WriteLine("Günaydın");
+                case 1:
+                    Console.WriteLine("Ocak ayı");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayı");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayı");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayı");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs ayı");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri");
+                    break;
             }
-            else if (time < 18)
+
+            switch (ay)
             {
-                Console.WriteLine("İyi Günler");
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Kış Mevsimi");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimi");
+                    break;
+                default:
+                    Console.WriteLine("Mevsim Belirsiz");
+                    break;
+
             }
-            else
-            {
-                Console.WriteLine("İyi Geceler");
-            }
-
-
-            string a = time >= 6 && time <= 11 ? "Günaydın!" : time <= 18 ? "İyi günler!" : "İyi geceler!";  
-
-
 
 
         
