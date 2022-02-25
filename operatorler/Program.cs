a153fa02e9d38471459049e2c87e00070f5b5dc4
ferhat-aslan <aslanferhat16@gmail.com>
2022-02-25 20:54:41 +0300

    dsfdes

--- operatorler/Program.cs
+++ operatorler/Program.cs
@@ -1,49 +1,25 @@
-﻿   Console.WriteLine("Lüften sayı giriniz ");
-            int a = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= a; i++)
+﻿ Console.WriteLine("Sayı giriniz :");
+            int a = int.Parse(Console.ReadLine());
+            int b = 1;
+            int c = 0;
+            while (b <= a)
             {
-                if (i % 2 == 1)
-                {
-                    Console.WriteLine(i);
-
-                }
-
+                c += b;
+                b++;
+            }
+            Console.WriteLine(c / a);
+            Console.WriteLine("--------------------------");
+            char e = 'a';
+            while (e < 'z')
+            {
+                Console.WriteLine(e);
+                e++;
 
             }
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("Yeni Örnek");
+            Console.WriteLine("--------------------------");
+            string[] ogrenciler = { "Ali", "Ahmet", "Ayşe", "Aylin" };
 
-            int tek = 0;
-            int çift = 0;
-            for (int i = 0; i <= 1000; i++)
+            foreach (var i in ogrenciler)
             {
-                if (i % 2 == 1)
-                {
-                    tek += i;
-                }
-
-                else
-                {
-                    çift += i;
-                }
-
-            }
-            Console.WriteLine("Tek sayılar toplamı :" + tek);
-            Console.WriteLine("Çift sayılar toplamı :" + çift);
-
-            Console.WriteLine("Yeni Örnek");
-
-            for (int i = 1; i < 10; i++)
-            {
-                if (i == 5)
-                    break;
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("Yeni Örnek");
-            for (int i = 1; i < 10; i++)
-            {
-                if (i == 5)
-                    continue;
-                Console.WriteLine(i);
+               Console.WriteLine(i);
             }
